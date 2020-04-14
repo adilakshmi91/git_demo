462a474514bcba0ec88b794cabafc9bb6df4cf01
adilakshmi91 <62950280+adilakshmi91@users.noreply.github.com>
2020-04-14 00:25:13 -0500

    Update Sample_code.java

--- src/Sample_code.java
+++ src/Sample_code.java
@@ -4,7 +4,7 @@ public class Sample_code {
 	public static void main(String[] args) {
 		
 		for ( int i=0;i<5;i++) {
-       System.out.println("print the message");
+      //print the message
        System.out.println("welcome to world");
 		}
 		for (int j=0;j<5;j++) {
