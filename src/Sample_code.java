61d1a8408fb3a07084a1b9e8862af8efebc54ab2
adilakshmi91 <adilakshmi64@gmail.com>
2020-04-13 23:43:18 -0500

    commenting was added

--- src/Sample_code.java
+++ src/Sample_code.java
@@ -4,7 +4,7 @@ public class Sample_code {
 	public static void main(String[] args) {
 		
 		for ( int i=0;i<5;i++) {
-	
+	//printing the statement 5 times
        System.out.println("welcome to world");
 		}
 	}
